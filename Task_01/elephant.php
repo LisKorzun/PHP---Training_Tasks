3d96c43bf20b3ce3d91de6c8c048cfe57c09df56
Olesya Korzun <liskorzun@gmail.com>
2015-04-25 13:58:02 +0300

    Reformat Task 1

--- Task_01/elephant.php
+++ Task_01/elephant.php
@@ -2,23 +2,22 @@
 
 <?php
 
-define("BLUE_COLOR", "\x1b[36m");
-define("DEL_COLOR", "\x1b[0m");
-define("BLUE_COLOR_WITH_BACKGROUND", "\x1b[34;47m");
+($argc <= 2) || die ("Please, enter only one name as parameter!!!\n");
+
+define("USER_NAME_DECOR", "\x1b[36m");  //blue text
+define("DEL_DECOR", "\x1b[0m");
+define("USER_MSG_DECOR", "\x1b[34;47m");  //blue text with white background
 define("NEW_LINE", "\n");
 
-if ($argc >2) {
-    die ('Please, enter only one name as parameter!!!'."\n");
-}
-elseif ($argc != 2) {
-    $name = 'Anonymous';
-} else {
-    $name = $argv[1];
-}
-printf('%1$s%4$s%2$s, buy an elephant!!!%3$s', BLUE_COLOR, DEL_COLOR, NEW_LINE, $name );
-$line = trim(fgets(STDIN));
-while (strcasecmp($line, 'exit') !== 0){
-    printf('%1$s%5$s%3$s, every can say %2$s"%6$s"%3$s and you take and buy an elephant!!!%4$s', BLUE_COLOR, BLUE_COLOR_WITH_BACKGROUND, DEL_COLOR, NEW_LINE, $name, $line );
-    $line = trim(fgets(STDIN));
+($argc != 2) ? $name = 'Anonymous': $name = $argv[1];
+
+printf('%1$s%4$s%2$s, buy an elephant!!!%3$s', USER_NAME_DECOR, DEL_DECOR, NEW_LINE, $name );
+$userMsg = trim(fgets(STDIN));
+
+$str = '%1$s%5$s%3$s, every can say %2$s"%6$s"%3$s and you take and buy an elephant!!!%4$s';
+$message = sprintf($str, USER_NAME_DECOR, USER_MSG_DECOR, DEL_DECOR, NEW_LINE, '%1$s', '%2$s');
+while (strcasecmp($userMsg, 'exit') !== 0){
+    printf($message, $name, $userMsg);
+    $userMsg = trim(fgets(STDIN));
 }
 ?>
