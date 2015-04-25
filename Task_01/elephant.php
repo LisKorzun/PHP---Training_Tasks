244075b0cc860c5052d17f12e955b40a2865dab3
Olesya Korzun <liskorzun@gmail.com>
2015-04-25 12:57:38 +0300

    Refactoring code of Task 1

--- Task_01/elephant.php
+++ Task_01/elephant.php
@@ -1,15 +1,24 @@
 #!/usr/bin/php5
 
 <?php
-if ($argc != 2) {
+
+define("BLUE_COLOR", "\x1b[36m");
+define("DEL_COLOR", "\x1b[0m");
+define("BLUE_COLOR_WITH_BACKGROUND", "\x1b[34;47m");
+define("NEW_LINE", "\n");
+
+if ($argc >2) {
+    die ('Please, enter only one name as parameter!!!'."\n");
+}
+elseif ($argc != 2) {
     $name = 'Anonymous';
 } else {
     $name = $argv[1];
 }
-echo "\x1b[36m".$name."\x1b[0m".', buy elephant!!!'."\n";
+printf('%1$s%4$s%2$s, buy an elephant!!!%3$s', BLUE_COLOR, DEL_COLOR, NEW_LINE, $name );
 $line = trim(fgets(STDIN));
 while (strcasecmp($line, 'exit') !== 0){
-    echo "\x1b[36m".$name."\x1b[0m". ', every can say '."\x1b[34;47m".'"'.$line.'"'."\x1b[0m".' and you take it and buy an elephant!!!'."\n";
+    printf('%1$s%5$s%3$s, every can say %2$s"%6$s"%3$s and you take and buy an elephant!!!%4$s', BLUE_COLOR, BLUE_COLOR_WITH_BACKGROUND, DEL_COLOR, NEW_LINE, $name, $line );
     $line = trim(fgets(STDIN));
 }
 ?>
