ea0254b8e87f42feea8c74b7c4e9e6d4047a1746
Olesya Korzun <liskorzun@gmail.com>
2015-08-11 23:12:44 +0300

    Reformat code

--- task_05/src/drawtable.php
+++ task_05/src/drawtable.php
@@ -2,51 +2,48 @@
 
 define("BUTTON_ADD", " <button class='add'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></button>");
 define("BUTTON_REMOVE", " <button class='remove'><span class='glyphicon glyphicon-minus' aria-hidden='true'></span></button>");
-define("TD", '<td rowspan="%1$d" class = "%2$s"> %3$s');
+define("TD", '<td rowspan="%1$d" class = "%2$s"> %3$s %4$s </td>');
 
 $file = 'data/source.json';
 $json = json_decode(file_get_contents($file), true);
 
 $html = '<h1 align="center">Table</h1>';
-$html .= '<table align="center" ><thead><tr><th class="role">Roles:</th><th class="group">Groups:</th><th class="option">Options:</th><th class="resource">Resources:</th></tr></thead>';
+$html .= '<table align="center" ><thead><tr>
+            <th class="role">Roles:</th>
+            <th class="group">Groups:</th>
+            <th class="option">Options:</th>
+            <th class="resource">Resources:</th>
+        </tr></thead>';
 
 foreach($json['roles'] as $role){
-    $rowRole = 0;
     if (count($role) > 1){
-        countRowSpan($role, $rowRole);
+        $rowRole = countRowSpan($role);
     }
-    $html .= sprintf(TD, $rowRole, "role", $role['title']);
-    $html .=  BUTTON_ADD.BUTTON_REMOVE. "</td>";
+    $html .= sprintf(TD, $rowRole, "role", $role['title'], BUTTON_ADD.BUTTON_REMOVE);
     if(isset ($role['groups'])){
         foreach($role['groups'] as $keyGroup => $group) {
-            $rowGroup = 0;
-            countRowSpan($group, $rowGroup);
+            $rowGroup =  countRowSpan($group);
             if ($keyGroup ===0){
-                $html .= sprintf(TD, $rowGroup, "group", $group['title']);
-                $html .=   BUTTON_ADD.BUTTON_REMOVE. "</td>";
+                $html .= sprintf(TD, $rowGroup, "group", $group['title'], BUTTON_ADD.BUTTON_REMOVE);
                 if (isset($group['options'])){
                     foreach($group['options'] as $keyOption => $option) {
-                        $rowOption = 0;
-                        countRowSpan($option, $rowOption);
+                        $rowOption =  countRowSpan($option);
                         if ($keyOption === 0) {
-                            $html .= sprintf(TD, $rowOption, "option", $option['title']);
-                            $html .=  BUTTON_ADD.BUTTON_REMOVE. "</td>";
+                            $html .= sprintf(TD, $rowOption, "option", $option['title'], BUTTON_ADD.BUTTON_REMOVE);
                             if (isset($option['resources'])){
                                 foreach($option['resources'] as $keyResource=> $resource) {
                                     $html .= ($keyResource === 0) ? '': '<tr>';
-                                    $html .= sprintf(TD, 1, "resource", $resource['title']);
-                                    $html .=  BUTTON_REMOVE. '</td>';
+                                    $html .= sprintf(TD, 1, "resource", $resource['title'], BUTTON_REMOVE);
                                     $html .= ($keyResource === 0) ? '': '</tr>';
                                 }
                             }
                         } else {
-                            $td = sprintf(TD, $rowOption, "option", $option['title']);
-                            $html .= '<tr>'.$td. BUTTON_ADD.BUTTON_REMOVE. "</td>";
+                            $td = sprintf(TD, $rowOption, "option", $option['title'], BUTTON_ADD.BUTTON_REMOVE);
+                            $html .= '<tr>'.$td;
                             if (isset($option['resources'])){
                                 foreach($option['resources'] as $keyResource=> $resource) {
                                     $html .= ($keyResource === 0) ? '': '<tr>';
-                                    $html .= sprintf(TD, 1, "resource", $resource['title']);
-                                    $html .= BUTTON_REMOVE. '</td>';
+                                    $html .= sprintf(TD, 1, "resource", $resource['title'], BUTTON_REMOVE);
                                     $html .= ($keyResource === 0) ? '': '</tr>';
                                 }
                             }
@@ -55,31 +52,25 @@ foreach($json['roles'] as $role){
                     }
                 }
             } else {
-                $td = sprintf(TD, $rowGroup, "group", $group['title']);
-                $html .= '<tr>'.$td. BUTTON_ADD.BUTTON_REMOVE. "</td>";
+                $html .= '<tr>'.sprintf(TD, $rowGroup, "group", $group['title'], BUTTON_ADD.BUTTON_REMOVE);
                 if (isset($group['options'])){
                     foreach($group['options'] as $keyOption => $option) {
-                        $rowOption = 0;
-                        countRowSpan($option, $rowOption);
+                        $rowOption = countRowSpan($option);
                         if ($keyOption === 0) {
-                            $html .= sprintf(TD, $rowOption, "option", $option['title']);
-                            $html .=  BUTTON_ADD.BUTTON_REMOVE. "</td>";
+                            $html .= sprintf(TD, $rowOption, "option", $option['title'], BUTTON_ADD.BUTTON_REMOVE);
                             if (isset($option['resources'])){
                                 foreach($option['resources'] as $keyResource=> $resource) {
                                     $html .= ($keyResource === 0) ? '': '<tr>';
-                                    $html .= sprintf(TD, 1, "resource", $resource['title']);
-                                    $html .= BUTTON_REMOVE. '</td>';
+                                    $html .= sprintf(TD, 1, "resource", $resource['title'], BUTTON_REMOVE);
                                     $html .= ($keyResource === 0) ? '': '</tr>';
                                 }
                             }
                         } else {
-                            $td = sprintf(TD, $rowOption, "option", $option['title']);
-                            $html .= '<tr>'.$td. BUTTON_ADD.BUTTON_REMOVE. "</td>";
+                            $html .= '<tr>'.sprintf(TD, $rowOption, "option", $option['title'], BUTTON_ADD.BUTTON_REMOVE);
                             if (isset($option['resources'])){
                                 foreach($option['resources'] as $keyResource=> $resource) {
                                     $html .= ($keyResource === 0) ? '': '<tr>';
-                                    $html .= sprintf(TD, 1, "resource", $resource['title']);
-                                    $html .= BUTTON_REMOVE. '</td>';
+                                    $html .= sprintf(TD, 1, "resource", $resource['title'], BUTTON_REMOVE);
                                     $html .= ($keyResource === 0) ? '': '</tr>';
                                 }
                             }
@@ -100,19 +91,20 @@ echo $html;
 /**
  * Считает количество rowSpan
  * @param array $data
- * @param $rowSpan
  */
-function countRowSpan(array $data, &$rowSpan)
+function countRowSpan(array $data)
 {
+    $rowSpan = 0;
     foreach($data as $key => $item){
         if($key === 'title'){
             if (count($data) == 1){
-                $rowSpan += count($data);
+                ++$rowSpan;
             }
             continue;
         }
         if(is_array($item)){
-            countRowSpan($item, $rowSpan);
+            $rowSpan += countRowSpan($item);
         }
     }
+    return $rowSpan;
 }
--- task_05/src/remove.php
+++ task_05/src/remove.php
@@ -1,4 +1,5 @@
 <?php
+
 if (isset($_POST['indexRole']) AND
     isset($_POST['indexGroup']) AND
     isset($_POST['indexOption']) AND
